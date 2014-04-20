a8c5b60ddafb31ac7a384575feff33458648a1dd
Tom MacWright <tom@macwright.org>
2014-04-20 13:59:07 -0400

    First

---
+++ README.md
@@ -0,0 +1,18 @@
+# extent
+
+a simple geographical extent
+
+## api
+
+### `extent()`
+
+Create a new extent object
+
+### `extent.include([lon, lat])`
+
+Expand the extent to include a lon, lat point.
+
+### `extent.bbox()`
+
+Get the extent's value. `null` if no points have
+been included yet.
---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = Extent;
+
+function Extent() {
+    if (!(this instanceof Extent)) {
+        return new Extent();
+    }
+    this._bbox = [Infinity, Infinity, -Infinity, -Infinity];
+    this._valid = false;
+}
+
+Extent.prototype.include = function(ll) {
+    this._valid = true;
+    this._bbox[0] = Math.min(this._bbox[0], ll[0]);
+    this._bbox[1] = Math.min(this._bbox[1], ll[1]);
+    this._bbox[2] = Math.max(this._bbox[2], ll[0]);
+    this._bbox[3] = Math.max(this._bbox[3], ll[1]);
+    return this;
+};
+
+Extent.prototype.bbox = function() {
+    if (!this._valid) return null;
+    return this._bbox;
+};
